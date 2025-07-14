39a72db038d0fbd09b7ca4de72fa12dd166b1bf8
hardik5838 <hderashri@asepeyo.es>
2025-07-14 13:24:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -250,6 +250,8 @@ def calculate_current(power_kw, voltage_v, phase_number, power_factor):
 
 st.set_page_config(page_title="Generador de Guía de Instalaciones Eléctricas", layout="centered")
 
+st.image("Logo_ASEPEYO.png", width=150) # Puedes ajustar el 'width' para cambiar el tamaño del logo
+
 st.title("⚡ Generador de Guía de Instalaciones Eléctricas")
 st.markdown("Genere requisitos eléctricos detallados según las normas de las compañías distribuidoras.")
 
